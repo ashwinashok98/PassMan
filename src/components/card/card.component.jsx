812c3dd19a4154e26f585f5d53055c1ca8f31d9b
ashwinashok98 <ashwinashok98@gmail.com>
2021-11-23 19:47:12 +0530

    Added Dialog modal for modification confirmation

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext } from "react";
 import { UserContext } from "../../context/userContext";
 import "./card.styles.scss";
 import { Modal, Button, Form } from "react-bootstrap";
-
+import DialogModal from "../dialog-modal/dialog-modal.component";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faEye,
@@ -20,6 +20,11 @@ const edit_btn = <FontAwesomeIcon icon={faEdit} />;
 const Card = ({ id, name, username, password }) => {
   const [passwordShown, setPasswordShown] = useState(false);
   const [edit, setEdit] = useState(false);
+
+  const [showDialog, setShowDialog] = useState(false); //Dialog box
+  const handleClose = () => setShowDialog(false);
+  const handleShow = () => setShowDialog(true);
+
   const [newUsername, setNewUsername] = useState(username);
   const [newPassword, setNewPassword] = useState(password);
   const [users, setUsers] = useContext(UserContext);
@@ -32,79 +37,84 @@ const Card = ({ id, name, username, password }) => {
     setEdit(!edit);
   };
   const confirmChange = () => {
-    console.log("In confirm change");
+    setShowDialog(true);
+    
+  };
+  const applyChange = () => {
     users.map((user) => {
       if (user.id === id) {
         user.username = newUsername;
         user.password = newPassword;
       }
     });
-    setUsers([...users])
+    setUsers([...users]);
   };
   const cancelChange = () => {
     setNewUsername(username);
     setNewPassword(password);
   };
- 
 
   return (
     <div className="card">
       <div class="card-title">
         {name.toUpperCase()}
-        
 
         <div className="underline-title"></div>
       </div>
       <div className="card-body">
-        
-          <Form.Group className="mb-3" controlId="cardUsername">
-            <Form.Label>Username</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter Username"
-              value={newUsername}
-              onChange={(e) => {
-                setNewUsername(e.target.value);
-              }}
-              disabled={!edit}
-            />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="cardPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type={passwordShown ? "text" : "password"}
-              placeholder="Password"
-              value={newPassword}
-              onChange={(e) => {
-                setNewPassword(e.target.value);
-              }}
-              disabled={!edit}
-            />
-            <i class="i-password" onClick={togglePasswordVisiblity}>
-              {passwordShown ? eye : eyeSlash}
-            </i>{" "}
-          </Form.Group>
+        <Form.Group className="mb-3" controlId="cardUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Username"
+            value={newUsername}
+            onChange={(e) => {
+              setNewUsername(e.target.value);
+            }}
+            disabled={!edit}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="cardPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type={passwordShown ? "text" : "password"}
+            placeholder="Password"
+            value={newPassword}
+            onChange={(e) => {
+              setNewPassword(e.target.value);
+            }}
+            disabled={!edit}
+          />
+          <i class="i-password" onClick={togglePasswordVisiblity}>
+            {passwordShown ? eye : eyeSlash}
+          </i>{" "}
+        </Form.Group>
 
-          <button class="edit-btn" onClick={toggleEdit}>
-            {edit ? (
-              <>
-                <FontAwesomeIcon
-                  icon={faCheck}
-                  className="confirm_btn"
-                  onClick={confirmChange}
-                />{" "}
-                <FontAwesomeIcon
-                  icon={faTimes}
-                  className="cancel_btn"
-                  onClick={cancelChange}
-                />
-              </>
-            ) : (
-              edit_btn
-            )}
-          </button>
-        
+        <button class="edit-btn" onClick={toggleEdit}>
+          {edit ? (
+            <>
+              <FontAwesomeIcon
+                icon={faCheck}
+                className="confirm_btn"
+                onClick={confirmChange}
+              />{" "}
+              <FontAwesomeIcon
+                icon={faTimes}
+                className="cancel_btn"
+                onClick={cancelChange}
+              />
+            </>
+          ) : (
+            edit_btn
+          )}
+        </button>
       </div>
+      <DialogModal
+        show={showDialog}
+        onHide={handleClose}
+        cancelChange={cancelChange}
+        applyChange={applyChange}
+      />
     </div>
   );
 };
---
+++ src/components/dialog-modal/dialog-modal.component.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Modal,Button } from "react-bootstrap";
+import "./dialog-modal.styles.scss";
+
+const DialogModal = (props) => {
+    const apply=()=>{
+        props.applyChange()
+        props.onHide()
+    };
+    const cancel=()=>{
+        props.cancelChange()
+        props.onHide()
+    };
+  return (
+    <Modal
+      {...props}
+      size="sm"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Body closeButton>
+        <h4>Are you sure to apply the changes?</h4>
+        <p>There would be no reverting once the changes are applied.</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="success" onClick={apply}>
+          Save Changes
+        </Button>
+        <Button variant="danger" onClick={cancel}>
+          Cancel
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+export default DialogModal;
